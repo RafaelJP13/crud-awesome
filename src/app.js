4daf77bd7f28aa55224b9cafb4287d63da3980fd
R <Rafaelsf013@gmail.com>
2021-07-05 12:57:33 -0300

    Add nodemon on start

--- src/app.js
+++ src/app.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
